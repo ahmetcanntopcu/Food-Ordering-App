538ff99e814f2b551732275d9388e732bfb0a992
Ahmet Can Topçu <ahmetcanntopcu@gmail.com>
2023-03-17 23:13:30 +0300

    home page setup added

---
+++ Food Ordering App/Views/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Food Ordering App
+//
+//  Created by Ahmet Can Topcu on 17.03.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+
+
+}
